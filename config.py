affce7a795a1c1049256888719f3cc9669ee340d
Marlay-0845 <marlayy0845@gmail.com>
2026-05-27 17:52:48 +0300

    fix config

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ OLLAMA_BASE_URL_V1 = 'http://127.0.0.1:11434/v1'
 LLM_MODEL = 'gemma3:4b'
 EMBEDDING_MODEL = 'hf.co/Casual-Autopsy/snowflake-arctic-embed-l-v2.0-gguf:Q4_K_M'
 RERANKING_MODEL = 'BAAI/bge-reranker-v2-m3'
-file_path = 'C:/Users/Daniil Work/.vscode/rag_tg_support1/rag_tg_project/rag/my_dict.json'
-persist_directory = 'C:/Users/Daniil Work/.vscode/rag_tg_support1/rag_tg_project/storage/chroma_db'
+file_path = 'C:/Users/User/.vscode/rag_tg_support1/rag_tg_project/rag/my_dict.json'
+persist_directory = 'C:/Users/User/.vscode/rag_tg_support1/rag_tg_project/storage/chroma_db'
 
 
 
